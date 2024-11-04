02a80fd70b82cbd9b418230b2590acb3c1d71cce
Mikołaj Linczewski <mlinczewski@deloittece.com>
2024-11-04 14:09:05 +0100

    init project

---
+++ addPlanner/addPlannerTests/addPlannerTests.swift
@@ -0,0 +1,17 @@
+//
+//  addPlannerTests.swift
+//  addPlannerTests
+//
+//  Created by Linczewski, Mikolaj on 04/11/2024.
+//
+
+import Testing
+@testable import addPlanner
+
+struct addPlannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
